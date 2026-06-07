8a5921b07bbae2116a81f9c1e8f2ad486eb6ebb0
Armando Sanchez <armando.san.2006@gmail.com>
2026-06-07 12:58:47 -0500

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,25 @@
+class MinStack:
+    def __init__(self):
+        self.st =[]
+        self.minSt =[]
+
+    def push(self, val: int) -> None:
+        self.st.append(val)
+        if not self.minSt or val <= self.minSt[-1]:
+            self.minSt.append(val)
+        else:
+            self.minSt.append(self.minSt[-1]) 
+    def pop(self) -> None:
+        if not self.st:
+            """if theres nothing then return nothing"""
+            return
+        self.minSt.pop()
+        self.st.pop() 
+    def top(self) -> int:
+        if not self.st:
+            return -1
+        return self.st[-1]
+    def getMin(self) -> int:
+        if not self.minSt:
+            return -1
+        return self.minSt[-1]
